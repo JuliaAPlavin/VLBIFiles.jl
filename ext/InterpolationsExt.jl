8e786cd23ecf58f6ba317294efe54370346c1b80
Alexander Plavin <alexander@plav.in>
2025-07-01 00:00:00 +0000

    extrapolation

--- ext/InterpolationsExt.jl
+++ ext/InterpolationsExt.jl
@@ -3,6 +3,6 @@ module InterpolationsExt
 using Interpolations
 using VLBIFiles
 
-VLBIFiles.image_stored(fimg::VLBI.FitsImage) = linear_interpolation(VLBI.image_stored(VLBIFiles.KeyedArray, fimg))
+VLBIFiles.image_stored(fimg::VLBI.FitsImage) = linear_interpolation(VLBI.image_stored(VLBIFiles.KeyedArray, fimg); extrapolation_bc=zero(eltype(fimg.data)))
 
 end
