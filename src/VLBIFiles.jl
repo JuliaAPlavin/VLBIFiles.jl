a7b0238438d1c58d0edcf865721d0b59a1d8d4b8
Alexander Plavin <alexander@plav.in>
2026-01-01 00:00:00 +0000

    move to src/fitsio

--- src/VLBIFiles.jl
+++ src/VLBIFiles.jl
@@ -24,9 +24,9 @@ using Dictionaries
 
 export VLBI, table, uvtable
 
-include("grouphdu.jl")
+include("fitsio/grouphdu.jl")
+include("fitsio/fitsutils.jl")
 
-include("fitsutils.jl")
 include("uvdata.jl")
 include("fitsimage.jl")
 include("difmap_files.jl")
