746ea9166fff825e8b2d898a27f820280dcb53c7
Alexander Plavin <alexander@plav.in>
2026-01-01 00:00:00 +0000

    fix?

--- ext/RectiGridsAxisKeysExtraExt.jl
+++ ext/RectiGridsAxisKeysExtraExt.jl
@@ -8,7 +8,7 @@ using VLBIFiles: FitsImage, SVector
 function VLBIFiles.image_clean(::Type{KeyedArray}, fimg::FitsImage; kwargs...)
     @eval InterferometricModels.exp_for_gaussintensity(x) = InterferometricModels.exp_for_gaussintensity_difmap(x)
     result = try
-        im = VLBI.image_clean(fimg; kwargs...)
+        im = @invokelatest VLBI.image_clean(fimg; kwargs...)
         im.(with_axiskeys(grid)(SVector, fimg.data))
     finally
         @eval InterferometricModels.exp_for_gaussintensity(x) = InterferometricModels.exp_for_gaussintensity_basic(x)
