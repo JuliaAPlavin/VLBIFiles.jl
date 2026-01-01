a48a066969b197a1bd4a37a3626c160f431846c5
Alexander Plavin <alexander@plav.in>
2026-01-01 00:00:00 +0000

    match difmap clean images better

--- ext/RectiGridsAxisKeysExtraExt.jl
+++ ext/RectiGridsAxisKeysExtraExt.jl
@@ -6,8 +6,14 @@ using VLBIFiles
 using VLBIFiles: FitsImage, SVector
 
 function VLBIFiles.image_clean(::Type{KeyedArray}, fimg::FitsImage; kwargs...)
-    im = VLBI.image_clean(fimg; kwargs...)
-    return im.(with_axiskeys(grid)(SVector, fimg.data))
+    @eval InterferometricModels.exp_for_gaussintensity(x) = InterferometricModels.exp_for_gaussintensity_difmap(x)
+    result = try
+        im = VLBI.image_clean(fimg; kwargs...)
+        im.(with_axiskeys(grid)(SVector, fimg.data))
+    finally
+        @eval InterferometricModels.exp_for_gaussintensity(x) = InterferometricModels.exp_for_gaussintensity_basic(x)
+    end
+    return result
 end
 
 end
