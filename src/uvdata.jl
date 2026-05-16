050e3024063683625e84e9f7c7f893d97ff9bf02
Paul Tiede <ptiede91@gmail.com>
2026-05-15 20:08:08 -0400

    fix str2int

--- src/uvdata.jl
+++ src/uvdata.jl
@@ -114,6 +114,7 @@ end
 
 strfloat_to_float(x::AbstractFloat) = x
 strfloat_to_float(x::String) = parse(Float64, replace(x, "D" => "E"))
+strfloat_to_float(x::Integer) = float(x)
 
 function AntArray(hdu::TableHDU)
     header = read_header(hdu)
